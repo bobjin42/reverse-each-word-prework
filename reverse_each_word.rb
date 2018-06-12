68bd93923e10dac7c8c3e1ea9beba39bb8d60c6f
Yicheng Jin <bobjin42@gmail.com>
2018-06-12 15:04:44 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   array = string.split(" ")
   new_array = []
-  array.each{|ele| new_array.unshift(ele)}
+  array.each{|ele| new_array.reverse(ele)}
   return new_array.join("")
 end
 
