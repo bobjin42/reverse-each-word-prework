053f1640e70748408cac8561bd5c8d3787b9dbdb
Yicheng Jin <bobjin42@gmail.com>
2018-06-12 15:04:50 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   array = string.split(" ")
   new_array = []
   array.each{|ele| new_array.reverse(ele)}
-  return new_array.join("")
+  return new_array.join(" ")
 end
 
   
