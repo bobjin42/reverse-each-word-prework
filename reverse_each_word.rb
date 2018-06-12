01635ebe2cabf766c8a4ad2ef0e2795ee10f2dc1
Yicheng Jin <bobjin42@gmail.com>
2018-06-12 15:10:39 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+def reverse_each_word(sentence)
+  array = sentence.split(" ")
+  new_array = []
+  array.each{|ele| new_array.push(ele.reverse)}
+  return new_array.join(" ")
+end
+
+def reverse_each_word(sentence)
+  array = sentence.split(" ")
+  return array.collect{|ele| ele.reverse}.join(" ")
+end
